e17f427bf27945e160f23a54af2f818c9a6dfbb0
Agentx49 <mathewbiltonen@gmail.com>
2025-10-06 02:50:07 -0400

    Demo ready ish
    updated unity
    added music
    added restaurant
    added logo
    added endings to dialogue

---
+++ Give Me A Hand/Assets/C# code/Yarn/Static YarnCommands.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using Yarn.Unity;
+
+
+public class Quit
+{
+    [YarnCommand("quitGame")]
+    public static void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+        //Just to make sure its working
+    }
+}
+
+public class Music
+{
+    [YarnCommand("musicOn")]
+    public static void MusicOn(AudioSource music)
+    {
+        music.Play();
+        Debug.Log("Playing " + music.name);
+    }
+
+    [YarnCommand("musicOff")]
+    public static void MusicOff(AudioSource music)
+    {
+        music.Stop();
+        Debug.Log("Stopping " + music.name);
+    }
+}
+
+public class Location
+{
+    static SpriteRenderer currentLocation;
+
+    [YarnCommand("Swap")]
+    public static void Swap(GameObject Location)
+    {
+        if (Location != null)
+        {
+            SpriteRenderer locationSprite = Location.GetComponent<SpriteRenderer>();
+            SpriteRenderer lastLocation;
+            
+
+            lastLocation = currentLocation;
+            currentLocation = locationSprite;
+            currentLocation.enabled = true;
+            Debug.Log(Location + " is now active");
+            if (lastLocation != null)
+            {
+                lastLocation.enabled = false;
+                Debug.Log(lastLocation + " is no longer active");
+            }
+        }
+        else Debug.LogWarning("GameObject not found for command 'Swap'.");
+    }
+}
