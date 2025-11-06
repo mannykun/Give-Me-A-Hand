629598a51eecd5d5e93e7f6a39b355dfa6b32993
Agentx49 <mathewbiltonen@gmail.com>
2025-11-05 23:59:34 -0500

    Added SuperUnityBuild Tool

---
+++ Give Me A Hand/Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,48 @@
+using System;
+
+// This file is auto-generated. Do not modify or move this file.
+
+namespace SuperUnityBuild.Generated
+{
+    public enum ReleaseType
+    {
+        None,
+        Main,
+    }
+
+    public enum Platform
+    {
+        None,
+        Windows,
+    }
+
+    public enum ScriptingBackend
+    {
+        None,
+        Mono,
+    }
+
+    public enum Target
+    {
+        None,
+        Player,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static class BuildConstants
+    {
+        public static readonly DateTime buildDate = new DateTime(638979835802001004);
+        public const string version = "1.0.0.1";
+        public const int buildCounter = 1;
+        public const ReleaseType releaseType = ReleaseType.Main;
+        public const Platform platform = Platform.Windows;
+        public const ScriptingBackend scriptingBackend = ScriptingBackend.Mono;
+        public const Target target = Target.Player;
+        public const Distribution distribution = Distribution.None;
+    }
+}
+
