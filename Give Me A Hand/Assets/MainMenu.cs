71171b011cd27e735ebdfeeff45b1c18c5c87178
Agentx49 <mathewbiltonen@gmail.com>
2025-12-08 09:36:36 -0500

    Credits menu

--- Give Me A Hand/Assets/MainMenu.cs
+++ Give Me A Hand/Assets/MainMenu.cs
@@ -6,18 +6,29 @@ public class MainMenu : MonoBehaviour
 {
     public Button StartButton;
     public Button QuitButton;
+    public Button CreditsButton;
+    public Button BackButton;
+
+    public GameObject CreditsPanel;
+
     public DialogueRunner dialogueRunner;
 
     void Start()
     {
         StartButton.onClick.AddListener(StartGame);
         QuitButton.onClick.AddListener(QuitGame);
+        CreditsButton.onClick.AddListener(OpenCredits);
+        BackButton.onClick.AddListener(CloseCredits);
+            CreditsPanel.SetActive(false);
+
     }
 
     void StartGame()
     {
         StartButton.gameObject.SetActive(false);
         QuitButton.gameObject.SetActive(false);
+        CreditsButton.gameObject.SetActive(false);
+
         dialogueRunner.StartDialogue("Start");
     }
 
@@ -28,4 +39,15 @@ public class MainMenu : MonoBehaviour
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
     }
+
+    void OpenCredits()
+    {
+        CreditsPanel.SetActive(true); 
+    }
+    
+    void CloseCredits()
+    {
+        CreditsPanel.SetActive(false);
+    }
+
 }
